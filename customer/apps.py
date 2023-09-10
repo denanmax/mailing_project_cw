b9484af1a745eee16b58de1ef8d21ebfeec60b1b
denanmax <deshis93@gmail.com>
2023-09-10 10:47:06 +0300

    customer created

--- customer/apps.py
+++ customer/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 
 
-class UsersConfig(AppConfig):
+class CustomerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
-
+    name = 'customer'
