62d6d4fa46797996469a3cb18680fdcb4096f6fa
denanmax <deshis93@gmail.com>
2023-09-10 08:55:05 +0300

    urls

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterUser, ProfileUser, ListUsersView, confirm_code, DeleteUserView, BlockUser
+from users.views import RegisterUser
 
 app_name = UsersConfig.name
 
@@ -10,9 +10,5 @@ urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('register/', RegisterUser.as_view(template_name='users/register.html'), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('list_users/', ListUsersView.as_view(), name='list_users'),
-    path('confirm_code/<str:email>/', confirm_code, name='confirm_code'),
-    path('delete/<int:pk>/', DeleteUserView.as_view(), name='delete'),
-    path('profile/', ProfileUser.as_view(), name='profile'),
-    path('blocked_user/<int:pk>/', BlockUser.as_view(), name='blocked_user'),
+
 ]
