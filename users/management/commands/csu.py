c8f9f345d4ed5013caa645a1ff1f66d8cfed26a8
denanmax <deshis93@gmail.com>
2023-09-10 08:51:01 +0300

    Initial commit

---
+++ users/management/commands/csu.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='admin@admin.com',
+            is_staff=True,
+            is_active=True,
+            is_superuser=True,
+            verify_code='0000'
+        )
+        user.set_password('z1243')
+        user.save()
